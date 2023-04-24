ca1804ed7936aeaa4cf3d763b6f80a608475c86f
odayvson <brenosantos1300@gmail.com>
2023-04-24 09:57:02 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+const key = "f200965144e4ae829add191f8efeb6cc"
+
+function colocarDadosNaTela(dados) {
+	document.querySelector(".cidade").innerHTML = dados.name
+	document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+	document.querySelector(".texto").innerHTML = dados.weather[0].description
+	document.querySelector(".umidade").innerHTML = "Umidade " + dados.main.humidity + "%"
+	document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+
+}
+
+async function buscarCidade(cidade) {
+
+	const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
+	colocarDadosNaTela(dados)
+}	
+
+function cliqueiNoBotao() {
+	const cidade = document.querySelector(".input-cidade").value
+
+	buscarCidade(cidade)
+}
+
+
+
